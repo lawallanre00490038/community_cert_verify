bf999bd8e98e789c3586a68d590bb6b314ee2560
Olanrewaju <lawallanre.abbahouse@gmail.com>
2024-07-14 19:42:30 +0100

    test

--- app/no_record/page.tsx
+++ app/no_record/page.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='flex justify-center items-center h-screen m-auto'>
-        <h1 className='text-2xl'>
+    <div className='flex justify-center items-center h-screen text-center'>
+        <h1 className='text-xl'>
             You do have a certificate. Contact the course Admin
         </h1>
     </div>
