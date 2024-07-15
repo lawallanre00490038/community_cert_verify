a050dc070c9aeb40dda8530edcd6e4afe506dc91
Olanrewaju <lawallanre.abbahouse@gmail.com>
2024-07-15 02:25:49 +0100

    test

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     
         <footer className="bg-white fixed bottom-0 right-0 left-0 z-50">
             <div>
-                <p className="text-center p-4 text-xs md:text-sm">All Rights Reserved. &copy; 2021 -
+                <p className="text-center p-4 text-xs md:text-sm">All Rights Reserved. &copy; 2024 -
                 <a href="#"><strong>Data Science Nigeria</strong></a></p>
             </div>
         </footer>
