14f2915b89a31db2a77501170da9f70675dc7152
Olanrewaju <lawallanre.abbahouse@gmail.com>
2024-07-31 11:50:53 +0100

    test

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -16,7 +16,7 @@ const NavBar = () => {
 
   return (
     <nav className='h-10 flex justify-between items-center my-4 px-4 md:px-0'>
-      <div className='flex justify-between items-center w-full px-8'>
+      <div className='flex justify-between items-center w-full'>
         <Link href="/dashboard" className='text-sm md:text-xl font-extrabold text-green-dsn py-4 rounded-xl max-w-[150px] md:max-w-full'>DSN Community Certificate Issuer</Link>
 
         <div className='hidden h-10 rounded-full border-2 bg-slate-100 md:flex items-center justify-center space-x-2 px-2 hover:cursor-pointer active:bg-slate-200 transition-all'>
