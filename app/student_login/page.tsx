7a40c7e1bf10b6ebf6b998c47c4a69af95ea126e
Olanrewaju <lawallanre.abbahouse@gmail.com>
2024-08-19 11:49:35 +0100

    test

--- app/student_login/page.tsx
+++ app/student_login/page.tsx
@@ -9,8 +9,12 @@ const StudentLogin = () => {
         
         <div className='md:grid grid-cols-2 h-full place-content-center'>
           <div className='hidden md:block w-full relative'>
-            <Image src='/bg1.png' alt='bg' width={500} height={500} className='h-full w-full z-0'/>
             <Image src='/logo.png' alt='bg' width={400} height={400} className='absolute translate-x-[50%] right-[50%]  top-[50%] -translate-y-[50%] z-50'/>
+            <Image className="h-full w-full object-cover rounded-md" src="/student_login.png" 
+            width={500} height={500}
+            alt="Random image" />
+              <div className="absolute inset-0 bg-green-200 opacity-60 rounded-md"></div>
+                  <Image src='/logo.png' alt='bg' width={400} height={400} className='absolute translate-x-[50%] right-[50%]  top-[50%] -translate-y-[50%] z-50'/>      
           </div>
           
           <div className='p-4 md:p-8 text-sm md:text-lg w-full max-w-[600px] place-self-center h-screen md:h-auto flex flex-col justify-center items-center md:block overflow-hidden'>
