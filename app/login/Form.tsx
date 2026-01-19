784768af2808f36f13f23ab26221b8e913f548e7
Olanrewaju <lawallanre.abbahouse@gmail.com>
2026-01-19 13:10:37 +0100

    done

--- app/login/Form.tsx
+++ app/login/Form.tsx
@@ -24,8 +24,8 @@ const FormSchema = z.object({
   email: z.string().email({
     message: "Invalid email address.",
   }),
-  password: z.string().min(6, {
-    message: "Password must be at least 6 characters."
+  password: z.string().min(5, {
+    message: "Password must be at least 5 characters."
   })
 })
 
